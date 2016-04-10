ebb56e07ad0a4498ff81243bf66e6a2ddd197806
NatsumiShimamoto <nshimamoto725@gmail.com>
2016-04-10 21:24:13 +0900

    first commit

--- Masuhara/ViewController.swift
+++ Masuhara/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var masuLabel : UILabel!
+    @IBOutlet var masuBtn : UIButton!
+    @IBOutlet var masuImgView : UIImageView!
+   
+    var masuAge : Int! = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        masuImgView.image = UIImage(named: "frame.png")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func masuPlus() {
+        masuAge = masuAge + 1
+        masuLabel.text = String(masuAge)
+        
+        if(masuAge == 0){
+            masuImgView.image = UIImage(named: "baby.png")
+        }
+        else if(masuAge == 10){
+            masuImgView.image = UIImage(named: "child.png")
+        }
+        else if(masuAge == 17){
+            masuImgView.image = UIImage(named: "teen.png")
+        }
+        else if(masuAge == 24){
+            masuImgView.image = UIImage(named: "adult.png")
+        }
+        else if(masuAge == 27){
+            masuImgView.image = UIImage(named: "now.png")
+            masuBtn.enabled = false
+        }
+    }
 }
 
